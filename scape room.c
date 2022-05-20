7bc305a7b490323569c7190db5942e0070e1c3de
miriam-estebanb <61233352+miriam-estebanb@users.noreply.github.com>
2022-05-20 13:23:45 +0200

    Update scape room.c

--- scape room.c
+++ scape room.c
@@ -26,27 +26,16 @@ void ComprobarDatosJugadores(char palabra1[20], char palabra2[20], contador){
 		}
 	}
 }*/
-char menu(){
-	
-	char opcion;
-	printf("Survival Horror-Un juego para los mas valientes\n Bienvenido al menu de opciones:\n");
-	printf("Introduce la opcion que desees:\n");
-	printf("************************\n");
-	printf("I-iniciar sesión para una nueva partida\n");
-	printf("R-Registrarse\n");
-	printf("X-Intrucciones del juego\n");
-	printf("S-Salir del juego\n");
-	printf("************************\n");
-	fflush(stdin);
-	scanf("%c",&opcion);
-	
-	return opcion;
-	
-}
 
-
+char menu();
+void Instrucciones(int ins);
+void cartelTitulo();
+void cartelIniciarSesion();
+void cartelRegistrarse();
+void cartelInstrucciones();
+void cartelSalida();
 int main(){
-	
+	cartelTitulo();
 	struct TJugador jugadores[MAX_JUG];
 	int i, contador, repetido;
 	char opcion;
@@ -58,7 +47,7 @@ int main(){
 		
 		switch (opcion){
 			
-			case 'I': printf("BIENVENIDO DE NUEVO\n INICIAR SESION:\n");
+			case 'I': cartelIniciarSesion();
 			         system("PAUSE");
 			
 			            //En primer lugar se abre el fichero
@@ -112,7 +101,8 @@ int main(){
 							
 						}while(repetido==0); //Pregunta hasta que encuentra un usuario y contraseña ya existentes
 						
-			case 'R': printf("Debes crear un nuevo usuario de jugador, siguiendo las instrucciones:\n");
+			case 'R': cartelRegistrarse();
+				  printf("\nDebes crear un nuevo usuario de jugador, siguiendo las instrucciones:\n");
 			                
 			         //Abrimos el fichero nuevamente
 			         pfichero=fopen("usuarios.txt","r");//En modo lectura para ver los usuarios ya existentes
@@ -184,13 +174,33 @@ int main(){
 					    system("PAUSE");
 					    break;
 				
-			case 'x': printf("Instrucciones:\n");
+			case 'x': cartelInstrucciones();
 				
 					Instrucciones(&ins);
 					break;
 					 
 					 
-				
+
+	} while (opcion!='S');
+}
+	
+char menu(){
+	
+	char opcion;
+	printf("Survival Horror-Un juego para los mas valientes\n Bienvenido al menu de opciones:\n");
+	printf("Introduce la opcion que desees:\n");
+	printf("************************\n");
+	printf("I-iniciar sesión para una nueva partida\n");
+	printf("R-Registrarse\n");
+	printf("X-Intrucciones del juego\n");
+	printf("S-Salir del juego\n");
+	printf("************************\n");
+	fflush(stdin);
+	scanf("%c",&opcion);
+	
+	return opcion;
+	
+}
 void Instrucciones(int ins){
 	
 	printf("SURVIVAL HORROR es un juego de preguntas donde el jugador se encarna en un grupo de amigos que quedan separados y deben ir avanzando a traves de diferentes caminos para poder lograr reencontrarse.\n La historia se divide en dos partes diferenciadas.\n\n");
@@ -199,19 +209,22 @@ void Instrucciones(int ins){
 	printf("Para acceder al juego es necesario crear un archivo .txt llamado "" en la carpeta del programa\n");
 					
 	do{
-		printf("Puede saber mas acerca del juego indicando una de las siguientes opciones:\n");
+		do
+		{
+			printf("Puede saber mas acerca del juego indicando una de las siguientes opciones:\n");
 		
-		printf("--------------------------------------------------------------------------\n");
+			printf("--------------------------------------------------------------------------\n");
 		
-		printf("1-Cultura General\n");
+			printf("1-Cultura General\n");
+			
+			printf("2-Supervivencia\n");
 		
-		printf("2-Supervivencia\n");
+			printf("3-Volver al Menu\n");
 		
-		printf("3-Volver al Menu\n");
+			fflush(stdin);
 		
-		//fflush(stdin);
-		
-		scanf("%d",&ins);
+			scanf("%d",&ins);
+		}while(ins!=1 && ins!=2 && ins!=3);
 					
 		if(ins=='1'){
 			printf("Cultura General\n");
@@ -229,17 +242,69 @@ void Instrucciones(int ins){
 		}else{
 			printf("Por favor, eliga una de las opciones validas\n");
 		}			
-}				         
-						
-						
-						
-			
-			
-			
-			
-			
-			
-				
-		}
-	} while (opcion!='S');
+	}
+	
+void cartelInstrucciones(){
+	printf("\n");
+	printf("\t\t\t\t\t\t.------..------..------..------..------..------..------..------..------..------..------..------..------.\n");
+	printf("\t\t\t\t\t\t|I.--. ||N.--. ||S.--. ||T.--. ||R.--. ||U.--. ||C.--. ||C.--. ||I.--. ||O.--. ||N.--. ||E.--. ||S.--. |\n");
+	printf("\t\t\t\t\t\t| :(): || :(): || :(): || :(): || :(): || :(): || :(): || :(): || :(): || :(): || :(): || :(): || :(): |\n");
+	printf("\t\t\t\t\t\t| ()() || ()() || ()() || ()() || ()() || ()() || ()() || ()() || ()() || ()() || ()() || ()() || ()() |\n");
+	printf("\t\t\t\t\t\t| '--'I|| '--'N|| '--'S|| '--'T|| '--'R|| '--'U|| '--'C|| '--'C|| '--'I|| '--'O|| '--'N|| '--'E|| '--'S|\n");
+	printf("\t\t\t\t\t\t`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'\n");
 }
+
+void cartelTitulo(){
+	printf("\n");
+	printf("\t\t\t .S    S.     sSSSSSSSs     .SSSSSs     .SSSSSs      sSSSSSSSs     .SSSSSs            sSSs   .S       S.    .SSSSSs     .S    S.    .S   .S    S.    .SSSSSs    S.      \n");
+	printf("\t\t\t.SS    SS.   dSSSP~YSSSb   .SS~YSSSb   .SS~YSSSb    dSSSP~YSSSb   .SSSSSSSb          dSSSP  .SS       SS.  .SSSSSSSb   .SS    SS.  .SS  .SS    SS.  .SSSSSSSS   SS.     \n");
+	printf("\t\t\tSSS    SSS  dSS'     `SSb  SSS   `SSb  SSS   `SSb  dSS'     `SSb  SSS   `SSb        dSS'    SSS       SSS  SSS   `SSb  SSS    SSS  SSS  SSS    SSS  SSS   SSSS  SSS     \n");
+	printf("\t\t\tSSS    SSS  SSS       SSS  SSS    SSS  SSS    SSS  SSS       SSS  SSS    SSS        SS|     SSS       SSS  SSS    SSS  SSS    SSS  SSS  SSS    SSS  SSS    SSS  SSS     \n");
+	printf("\t\t\tSSS SSSSSS  SSS       SSS  SSS    dSS  SSS    dSS  SSS       SSS  SSS    dSS        SSS     SSS       SSS  SSS    dSS  SSS    SSS  SSS  SSS    SSS  SSS SSSSSS  SSS     \n");
+	printf("\t\t\tSSS  SSSSS  SSS       SSS  SSS   .SSS  SSS   .SSS  SSS       SSS  SSS   .SSS        YSSs    SSS       SSS  SSS   .SSS  SSS    SSS  SSS  SSS    SSS  SSS  SSSSS  SSS     \n");
+	printf("\t\t\tSSS    SSS  SSS       SSS  SSS_sdSSS   SSS_sdSSS   SSS       SSS  SSS_sdSSS         `SSSS   SSS       SSS  SSS_sdSSS   SSS    SSS  SSS  SSS    SSS  SSS    SSS  SSS     \n");
+	printf("\t\t\tSSS    SSS  SSS       SSS  SSS~YSSSb   SSSSYSSSb   SSS       SSS  SSSSYSSSb           `SSS  SSS       SSS  SSSSYSSSb   SSS    SSS  SSS  SSS    SSS  SSS    SSS  SSS     \n");
+	printf("\t\t\tSSS    SSS  SSb       dSS  SSS   `SSb  SSS   `SSb  SSb       dSS  SSS   `SSb           lSS  SSb       dSS  SSS   `SSb  SSS    SSS  SSS  SSb    SSS  SSS    SSS  SSS     \n");
+	printf("\t\t\tSSS    SSS  SSS.     .SSS  SSS    SSS  SSS    SSS  SSS.     .SSS  SSS    SSS          .SSP  SSS.     .SSS  SSS    SSS  SSS.   SSS  SSS  SSS.   SSS  SSS    SSS  SSS.    \n");
+	printf("\t\t\tSSS    SSS   SSSbs_sdSSS   SSS    SSS  SSS    SSS   SSSbs_sdSSS   SSS    SSS        sSSSS    SSSbs_sdSSS   SSS    SSS   SSSbs_SSS  SSS   SSSbs_SSS  SSS    SSS   SSSbs  \n");
+	printf("\t\t\tSSS    SSS    YSSPSSSSY    SSS    SSS  SSS    SSS    YSSPSSSSY    SSS    SSS        YSS'      YSSPSSSSY    SSS    SSS    YSSSSSSS  SSS    YSSSSSSS  SSS    SSS    YSSP  \n");
+	printf("\t\t\t       SP                  SP          SP                         SP                                       SP                      SP                      SP           \n");
+	printf("\t\t\t       Y                   Y           Y                          Y                                        Y                       Y                       Y            \n");
+}
+
+void cartelSalida(){
+	printf("\n");
+	printf("\t\t\t\t\t\t\t\t\t\t\t         (     (        )   (     \n");
+	printf("\t\t\t\t\t\t\t\t\t\t\t   (     )%c )  )%c )  ( /(   )%c )  \n",92,92,92);
+	printf("\t\t\t\t\t\t\t\t\t\t\t   )%c   (()/( (()/(  )%c()) (()/(  \n",92,92);
+	printf("\t\t\t\t\t\t\t\t\t\t\t((((_)(  /(_)) /(_))((_)%c   /(_)) \n",92);
+	printf("\t\t\t\t\t\t\t\t\t\t\t )%c _ )%c(_))_ (_))    ((_) (_))   \n",92,92);
+	printf("\t\t\t\t\t\t\t\t\t\t\t (_)_%c(_)|   %c|_ _|  / _ %c / __|  \n",92,92,92);
+	printf("\t\t\t\t\t\t\t\t\t\t\t  / _ %c  | |) || |  | (_) |%c__ %c  \n",92,92,92);
+	printf("\t\t\t\t\t\t\t\t\t\t\t /_/ %c_%c |___/|___|  %c___/ |___/  \n",92,92,92);
+}
+
+void cartelRegistrarse(){
+	printf("\n");
+	printf("\t\t\t\t\t\t\t   (`-')  (`-')  _            _      (`-').->(`-')        (`-')  (`-')  _    (`-')  (`-').->(`-')  _ \n");
+	printf("\t\t\t\t\t\t\t<-.(OO )  ( OO).-/    .->    (_)     ( OO)_  ( OO).->  <-.(OO )  (OO ).-/ <-.(OO )  ( OO)_  ( OO).-/ \n");
+	printf("\t\t\t\t\t\t\t,------,)(,------. ,---(`-') ,-(`-')(_)--%c_) /    '._  ,------,) / ,---.  ,------,)(_)--%c_)(,------. \n",92,92);
+	printf("\t\t\t\t\t\t\t|   /`. ' |  .---''  .-(OO ) | ( OO)/    _ / |'--...__)|   /`. ' | %c /`.%c |   /`. '/    _ / |  .---' \n",92,92);
+	printf("\t\t\t\t\t\t\t|  |_.' |(|  '--. |  | .-, %c |  |  )%c_..`--. `--.  .--'|  |_.' | '-'|_.' ||  |_.' |%c_..`--.(|  '--.  \n",92,92,92);
+	printf("\t\t\t\t\t\t\t|  .   .' |  .--' |  | '.(_/(|  |_/ .-._)   %c   |  |   |  .   .'(|  .-.  ||  .   .'.-._)   %c|  .--'  \n",92,92);
+	printf("\t\t\t\t\t\t\t|  |%c  %c  |  `---.|  '-'  |  |  |'->%c       /   |  |   |  |%c  %c  |  | |  ||  |%c  %c %c       /|  `---. \n",92,92,92,92,92,92,92,92);
+	printf("\t\t\t\t\t\t\t`--' '--' `------' `-----'   `--'    `-----'    `--'   `--' '--' `--' `--'`--' '--' `-----' `------' \n");
+}
+
+void cartelIniciarSesion(){
+	printf("\n");
+	printf("\t\t\t\t\t  _     <-. (`-')_  _                 _     (`-')  _    (`-')      (`-').->(`-')  _ (`-').->  _                <-. (`-')_ \n");
+	printf("\t\t\t\t\t (_)       %c( OO) )(_)     _         (_)    (OO ).-/ <-.(OO )      ( OO)_  ( OO).-/ ( OO)_   (_)         .->      %c( OO) )\n",92);
+	printf("\t\t\t\t\t ,-(`-'),--./ ,--/ ,-(`-') %c-,-----. ,-(`-')/ ,---.  ,------,)    (_)--%c_)(,------.(_)--%c_)  ,-(`-')(`-')----. ,--./ ,--/ \n",92,92,92);
+	printf("\t\t\t\t\t | ( OO)|   %c |  | | ( OO)  |  .--./ | ( OO)| %c /`.%c |   /`. '    /    _ / |  .---'/    _ /  | ( OO)( OO).-.  '|   %c |  | \n",92,92,92,92);
+	printf("\t\t\t\t\t |  |  )|  . '|  |)|  |  ) /_) (`-') |  |  )'-'|_.' ||  |_.' |    %c_..`--.(|  '--. %c_..`--.  |  |  )( _) | |  ||  . '|  |)\n",92,92);
+	printf("\t\t\t\t\t(|  |_/ |  |%c    |(|  |_/  ||  |OO )(|  |_/(|  .-.  ||  .   .'    .-._)   %c|  .--' .-._)   %c(|  |_/  %c|  |)|  ||  |%c    | \n",92,92,92,92,92);
+	printf("\t\t\t\t\t |  |'->|  | %c   | |  |'->(_'  '--'%c |  |'->|  | |  ||  |%c  %c     %c       /|  `---.%c       / |  |'->  '  '-'  '|  | %c   | \n",92,92,92,92,92,92,92);
+	printf("\t\t\t\t\t `--'   `--'  `--' `--'      `-----' `--'   `--' `--'`--' '--'     `-----' `------' `-----'  `--'      `-----' `--'  `--' \n");
+}
+	
